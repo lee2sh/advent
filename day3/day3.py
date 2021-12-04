ad01f24a9a2c8d5afed4474d7071fa7909e6e51b
Jaden Lee <jlee4@2u.com>
2021-12-04 02:02:01 -0500

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -12,29 +12,62 @@ def getGamma(bn):
             gamma += "1" 
         else:
             gamma += "0"
-
     res = int(gamma, 2)
-    print(res)
-    return res
+    return (res, gamma)
 
-def getEpsilon(bn):
-    ep = ""
-    for i in range(len(bn[0].strip("\n"))):
-        count1 = 0
-        count0 = 0
-        for j in range(len(bn)):
-            if bn[j][i] == "1":
-                count1 += 1
-            else:
-                count0 += 1
-        if count1 < count0:
-            ep += "1" 
+
+def getEp(gamma):
+    gs = str(gamma)
+    res = ""
+    for s in gs:
+        if s == "1":
+            res += "0"
         else:
-            ep += "0"
+            res += "1"
+    return (int(res, 2), res)
 
-    res = int(ep, 2)
-    print(res)
+def findPosition(bn, position, pn):
+    res = []
+    for i in range(len(bn)):
+        if bn[i][position] == pn:
+            res.append(bn[i])
     return res
+
+def findCommonBit(bn, position):
+    count1 = count0 = 0
+    for i in range(len(bn)):
+        if bn[i][position] == "1":
+            count1 += 1
+        else:
+            count0 += 1
+
+    return "1" if count1 >= count0 else "0" 
+
+    
+def findUncommonBit(bn, position):
+    count1 = count0 = 0
+    for i in range(len(bn)):
+        if bn[i][position] == "1":
+            count1 += 1
+        else:
+            count0 += 1
+
+    return "0" if count0 <= count1 else "1" 
+
+def getLevels(bn):
+    ores = bn
+    cres = bn
+    for i in range(len(bn[0].strip("\n"))):
+        ## find common/uncommon next bit
+        mostCommonBit = findCommonBit(ores, i)
+        mostUncommonBit = findUncommonBit(cres, i)
+        if len(ores) == 1 and len(cres) == 1:
+            break
+        if len(ores) > 1:
+            ores = findPosition(ores, i, mostCommonBit) 
+        if len(cres) > 1:
+            cres = findPosition(cres, i, mostUncommonBit)
+    return (ores[0], cres[0])
 
 def importInput(inputFile):
     binary_nums = []
@@ -45,9 +78,12 @@ def importInput(inputFile):
 
 def main():
     bn = importInput("./input")
-    gamma = getGamma(bn)
-    epsilon = getEpsilon(bn)
-    print("first part answer: " + str(gamma*epsilon))
+    gres, gamma = getGamma(bn)
+    eres, ep = getEp(gamma)
+    print("first part answer: " + str(gres*eres))
+    goxy, gco2 = getLevels(bn)
+    ans2 = int(goxy.strip("\n"), 2) * int(gco2.strip("\n"), 2)
+    print("second part answer: " + str(ans2))
 
 if __name__ == '__main__':
     main()
